5ccfd119bad23c030bb20a183c9ec55418ddcd36
Hazboun6 <jeffrey.hazboun@gmail.com>
2018-03-13 12:49:55 -0700

    first commit

---
+++ README.rst
@@ -0,0 +1,37 @@
+============
+tabletop_pta
+============
+
+
+.. image:: https://img.shields.io/pypi/v/tabletop_pta.svg
+        :target: https://pypi.python.org/pypi/tabletop_pta
+
+.. image:: https://img.shields.io/travis/Hazboun6/tabletop_pta.svg
+        :target: https://travis-ci.org/Hazboun6/tabletop_pta
+
+.. image:: https://readthedocs.org/projects/tabletop-pta/badge/?version=latest
+        :target: https://tabletop-pta.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+
+
+Python Package to do Time of Arrival calculations for a Pulsar Timing Array demmo with metronomes.
+
+
+* Free software: MIT license
+* Documentation: https://tabletop-pta.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Jeffrey Shafiq Hazboun",
+    author_email='jeffrey.hazboun@nanograv.org',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Python Package to do Time of Arrival calculations for a Pulsar Timing Array demmo with metronomes.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='tabletop_pta',
+    name='tabletop_pta',
+    packages=find_packages(include=['tabletop_pta']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Hazboun6/tabletop_pta',
+    version='0.1.0',
+    zip_safe=False,
+)
