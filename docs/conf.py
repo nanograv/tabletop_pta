2321bc97400877a15b9d06a68a9f24e1f61dc5a7
Hazboun6 <jeffrey.hazboun@gmail.com>
2018-05-31 09:40:11 -0700

    Changed import order of package.

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import tabletop_pta
-
 from unittest.mock import MagicMock
 
 class Mock(MagicMock):
@@ -34,6 +32,7 @@ class Mock(MagicMock):
 MOCK_MODULES = ['pyaudio']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
+import tabletop_pta
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
